715a1a51ffbfe069e3a6b79278f0697401556338
Clayton Pereira <clayton.pereira@zup.com.br>
2021-06-27 21:52:31 -0300

    Project - SWAGGER and SpringServer Admin for Monotoring Implemented

--- forum/src/main/java/br/com/alura/forum/ForumApplication.java
+++ forum/src/main/java/br/com/alura/forum/ForumApplication.java
@@ -5,9 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
 @EnableCaching //Habilita o Springa para utilização de Cache
 @EnableSpringDataWebSupport //Habilita acesso dos parametros de paginacao do Pageable
+@EnableSwagger2 //Documentação automatica
 public class ForumApplication {
 
 	public static void main(String[] args) {
--- forum/src/main/java/br/com/alura/forum/config/security/SecurityConfiguration.java
+++ forum/src/main/java/br/com/alura/forum/config/security/SecurityConfiguration.java
@@ -19,49 +19,49 @@ import br.com.alura.forum.repository.UsuarioRepository;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-	
+
 	@Autowired
 	private AutenticacaoService autenticacaoService;
-	
+
 	@Autowired
 	private TokenService tokenService;
-	
+
 	@Autowired
 	private UsuarioRepository usuarioRepository;
-	
+
 	@Bean
 	@Override
 	protected AuthenticationManager authenticationManager() throws Exception {
 		return super.authenticationManager();
 	}
-	
-	//Configura Autenticações
+
+	// Configura Autenticações
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(autenticacaoService).passwordEncoder(new BCryptPasswordEncoder());
 	}
-	
-	//Configura Autorizações
+
+	// Configura Autorizações
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
 		.antMatchers(HttpMethod.POST, "/auth").permitAll()
 		.antMatchers(HttpMethod.GET, "/topicos").permitAll()
 		.antMatchers(HttpMethod.GET, "/topicos/*").permitAll()
+		.antMatchers(HttpMethod.GET, "/actuator/**").permitAll()
 		.anyRequest().authenticated()
-		//.and().formLogin() //Cria a sessão e fere o principio REST Stateless
-		.and().csrf().disable() //crsf-ataque hacker quando altenticação é via Token
-		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) //principio REST Stateless OK
-		.and().addFilterBefore(new AutenticacaoViaTokenFilter(tokenService, usuarioRepository), UsernamePasswordAuthenticationFilter.class); 
+	 // .and().formLogin() //Cria a sessão e fere o principio REST Stateless
+		.and().csrf().disable() // crsf-ataque hacker quando altenticação é via Token
+		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // principio REST Stateless OK
+		.and().addFilterBefore(new AutenticacaoViaTokenFilter(tokenService, usuarioRepository), UsernamePasswordAuthenticationFilter.class);
 	}
-	
-	//Configura recursos estáticos(js, css, imagens,etc.)
-	@Override
-	public void configure(WebSecurity web) throws Exception {
-		
-	}
-	
-	
+
+	//Configuracoes de recursos estaticos(js, css, imagens, etc.)
+		@Override
+		public void configure(WebSecurity web) throws Exception {
+			web.ignoring().antMatchers("/**.html", "/v2/api-docs", "/webjars/**", "/configuration/**", "/swagger-resources/**");
+		}
+
 //	//Métedo auxiliar para encriptar uma senha
 //	public static void main(String[] args) {
 //		System.out.println(new BCryptPasswordEncoder().encode("123456"));
